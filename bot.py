b6af37cd9a111f96885cbf8f6c8cd9417f9d973f
nogarremi <nogarremi@nogarremi.com>
2020-07-13 18:44:23 -0700

    Beautification in 3 parts
    Part 1
    
    In an attempt to clean up, I have seperated out functions based on: https://github.com/ellipses/Yaksha
    
    Part 2
    
    Commands work. No comments.
    
    Part 3
    
    Comments and some reordering
    
    Part 3.1
    
    I forgot pingtest and updated ReadMe
    
    Part 3.2
    
    I forgot the remind command
    `- Nog`

---
+++ bot.py
@@ -0,0 +1,74 @@
+import discord
+import os
+import json
+
+import interface
+from utilities import read_db,settings_exist
+from secret import token
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+    print('-------------------------------')
+    for guild in client.guilds:
+        print('Joined guild %s' % guild)
+
+
+# Yaksha
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    # Check if the channel is in the DB
+    # Add it if it isn't
+    if not settings_exist(message.guild.id, message.channel.id):
+        await chan.send("Oops, I'm broken")
+        print("Oops, I'm broken")
+
+    # Get prefix for the guild
+    prefix = read_db('guild', 'prefix-lizard', message.guild.id)
+
+    if not message.content.startswith(prefix):
+        return
+
+    for command in client.commands.keys():
+        msg = message.content # The message
+        user = message.author # The author
+
+        # Check if the message begins with a command
+        if msg[1:].lower().startswith(command.lower()):
+            # Remove the command from the start
+            msg = msg[len(command)+1:].strip()
+            command = command.lower()
+            
+            # Await the interface calling the command
+            response = await client.interface.call_command(command, msg, user, message.channel, guild=message.guild.id, full_msg=message)
+            
+            # If there is a response, send it
+            if response:
+                await message.channel.send(response)
+            break
+
+# Yaksha
+def main():
+    # Pull in a seperate config
+    config = json.loads(open(os.path.join(os.path.dirname(__file__), './bots.json')).read())
+
+    # Grab our commands
+    client.commands = config.get('common_commands', {}).copy()
+    client.commands.update(config.get('admin_commands', {}))
+
+    client.config = config
+
+    # Start our interface for our commands and Discord
+    client.interface = interface.Interface(config, client.commands)
+
+    # Start the bot
+    client.run(token)
+
+# If main is here, run main
+if __name__ == '__main__':
+    main()
