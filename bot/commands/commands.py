61b92688f12d9fb76e26c91625f12f824a10e300
nogarremi <nogarremi@nogarremi.com>
2020-09-12 10:07:02 -0700

    Quality of Life improvements
    Added shorthand commands, additional return messages for errors, and moved prefix-lizard to common commands
    `- Nog`

--- bot/commands/commands.py
+++ bot/commands/commands.py
@@ -15,28 +15,38 @@ from commands.utilities import (register, bold, get_users, is_channel, pings_b_g
 
 @register('bracket')
 async def bracket(command, msg, user, channel, *args, **kwargs):
-    return read_db('channel', command, channel.id)
+    return read_db('channel', 'bracket', channel.id)
 
 @register('help-lizard')
+@register('helpliz')
 async def help_lizard(command, msg, user, channel, *args, **kwargs):
-    return "For more information about the bot and its commands: https://github.com/lizardman301/Lizard-bot-rsf"
+    return "For more information about the bot and its commands: <https://github.com/lizardman301/Lizard-bot-rsf>"
 
 @register('lizardman')
+@register('ping')
+@register('liz')
 async def ping(command, msg, user, channel, *args, **kwargs):
     print("Pinged by {0}".format(user))
     return "Fuck you, Lizardman"
 
 @register('pingtest')
+@register('pt')
 async def pingtest(command, msg, user, channel, *args, **kwargs):
-    return "To initiate a ping test, both players go to https://testmyspeed.onl/ and choose a common server between each players location. Send the results of both tests to the TO."
+    return "To initiate a ping test, both players go to <https://testmyspeed.onl/> and choose a common server between each players location. Send the results of both tests to the TO."
 
-@register('random')
+@register('prefix-lizard')
+@register('prefliz')
+async def prefix(command, msg, user, channel, *args, **kwargs):
+    return "The prefix is: {0}".format(read_db('guild', 'prefix-lizard', kwargs['guild']))
+
 @register('randomselect')
+@register('random')
+@register('rs')
 async def randomselect(command, msg, user, channel, *args, **kwargs):
     # Selects and returns a random character from the given list of sfv characters
     # Accurate as of 9/11/2020, sfv version 5.031
     sfvchars = ["Ryu","Chun-Li","Nash","M. Bison (Dictator)","Cammy","Birdie","Ken","Necalli","Vega (Claw)","R. Mika","Rashid","Karin","Zangief","Laura","Dhalsim","F.A.N.G","Alex","Guile","Ibuki","Balrog (Boxer)","Juri","Urien","Akuma","Kolin","Ed","Abigail","Menat","Zeku","Sakura","Blanka","Falke","Cody","G","Sagat","Kage","E. Honda","Lucia","Poison","Gill", "Seth"]
-    return "{0} Your randomly selected character is: {1}".format(user.mention,bold(random.choice(sfvchars)))
+    return "{0} Your randomly selected character is: {1}".format(user.mention, bold(random.choice(sfvchars)))
 
 @register('status')
 async def status(command, msg, user, channel, *args, **kwargs):
@@ -44,37 +54,42 @@ async def status(command, msg, user, channel, *args, **kwargs):
     if currentRound:
         # Read the status message for a channel and make it bold
         # Currently the message must have {0} so it can fill in the current round
-        return bold(read_db('channel', command, channel.id).format(currentRound))
+        return bold(read_db('channel', 'status', channel.id).format(currentRound))
     return bold("Tournament has not begun. Please wait for the TOs to start Round 1!")
 
 @register('stream')
 async def stream(command, msg, user, channel, *args, **kwargs):
-    return read_db('channel', command, channel.id)
+    return read_db('channel', 'stream', channel.id)
 
 @register('tos')
 async def TOs(command, msg, user, channel, *args, **kwargs):
-    tos = read_db('channel', command, channel.id)
+    tos = read_db('channel', 'tos', channel.id)
     # If we get a value back, return TOs
     if tos:
         return tos
-    return "Oops, there is no TO associated with this channel. Please try somewhere else."
+    return "There are no TOs associated with this channel."
 
 # Admin Commands
 
 @register('botrole')
+@register('role')
 async def botrole(command, msg, user, channel, *args, **kwargs):
     # Pull the role name from the user's roles
     for role in user.roles:
-        if role.id == read_db('guild', command, kwargs['guild']):
+        if role.id == read_db('guild', 'botrole', kwargs['guild']):
             return "The bot role is {0}".format(bold(role.name))
 
 @register('challonge')
+@register('chal')
 async def challonge(command, msg, user, channel, *args, **kwargs):
+    if len(msg.split(' ')) < 2:
+        return "Lack of arguments. " + await help_lizard(0,0,0,0)
+
     async with channel.typing():
         base_url = "https://api.challonge.com/v1/tournaments/" # Base url to access Challonge's API
         subcommand = msg.split(' ')[0].lower() # The function trying to be accomplished
         tour_url = msg.split(' ')[1] # Bracket to pull from
-        subdomain = read_db('guild', command, kwargs['guild']) # Server's subdomain with Challonge
+        subdomain = read_db('guild', 'challonge', kwargs['guild']) # Server's subdomain with Challonge
 
         # Properly add the subdomain to the bracket url
         if subdomain:
@@ -86,24 +101,24 @@ async def challonge(command, msg, user, channel, *args, **kwargs):
             parts = parts_get.json() # Convert response from json to Python Dictionary
 
             # If Checkin
-            if subcommand in 'checkin':
+            if subcommand == 'checkin':
                 not_checked_in_parts, not_discord_parts = checkin(parts, get_users(kwargs['full_msg']))
 
                 # Message showing who is not checked in and who is not in the Discord
                 return_msg = "**NOT CHECKED IN:** {0}\n**NOT IN DISCORD:** {1}".format(', '.join(not_checked_in_parts), ', '.join(not_discord_parts))
 
             # If Seeding
-            elif subcommand in 'seeding':
+            elif subcommand == 'seeding':
                 # If msg has 3 params left 3rd one must be seed number
                 # Else, seed whole bracket
                 seed_num = int(msg.split(' ')[2]) if len(msg.split(' ')) > 2 else 0
 
                 # Get Google Sheets ID
-                sheet_id = read_db('channel', subcommand, channel.id)
+                sheet_id = read_db('channel', 'seeding', channel.id)
 
                 # If seeding hasn't been set, inform user
                 if not sheet_id:
-                    return "There is no seeding sheet for this channel. Please view https://github.com/lizardman301/Lizard-bot-rsf/blob/master/doc/seeding_with_sheets.md for a walkthrough"
+                    return "There is no seeding sheet for this channel. Please view <https://github.com/lizardman301/Lizard-bot-rsf/blob/master/doc/seeding_with_sheets.md> for a walkthrough"
 
                 seeds = seeding(sheet_id, parts, base_url + '/' + tour_url,seed_num)
                 if isinstance(seeds, str):
@@ -126,6 +141,8 @@ async def challonge(command, msg, user, channel, *args, **kwargs):
             print(parts_get.text)
 
 @register('coin-flip')
+@register('flip')
+@register('cf')
 async def coin_flip(command, msg, user, channel, *args, **kwargs):
     flip = "The coin landed on: {0}"
     # Flip a coin
@@ -152,6 +169,9 @@ async def edit(command, msg, user, channel, *args, **kwargs):
             params.remove(command_channels[chnl]) # Remove the channel from the params
 
     editable_command = params[0].lower() # Lower the command we are editing
+    if editable_command not in kwargs['edit_subs']:
+        return "Invalid Subcommand. " + await help_lizard(0,0,0,0)
+
     params.remove(editable_command) # Remove the command from the params
 
     # Rejoin the rest of the parameters with spaces
@@ -173,7 +193,7 @@ async def edit(command, msg, user, channel, *args, **kwargs):
     elif editable_command in ['seeding']:
         reg = re.compile('[a-zA-Z0-9-_]+')
         if not reg.fullmatch(params[0]):
-            return "Invalid Sheets spreadsheet ID. Please view https://github.com/lizardman301/Lizard-bot-rsf/blob/master/doc/seeding_with_sheets.md for a walkthrough"
+            return "Invalid Sheets spreadsheet ID. Please view <https://github.com/lizardman301/Lizard-bot-rsf/blob/master/doc/seeding_with_sheets.md> for a walkthrough"
 
     # Check for guild settings, channel settings, or multi channel settings
     if editable_command in ['botrole', 'challonge','prefix-lizard']:
@@ -189,10 +209,6 @@ async def edit(command, msg, user, channel, *args, **kwargs):
         save_db('channel', editable_command, db_message, channel.id) # Save the new message to the proper setting in a given channel
 
     return "The new {0} is: {1}".format(bold(editable_command), bold(channel_message)) # Print the new message for a given setting
-
-@register('prefix-lizard')
-async def prefix(command, msg, user, channel, *args, **kwargs):
-    return "The prefix is: {0}".format(read_db('guild', command, kwargs['guild']))
 
 @register('refresh')
 async def refresh(command, msg, user, channel, *args, **kwargs):
@@ -233,5 +249,5 @@ async def reset(command, msg, user, channel, *args, **kwargs):
 
 @register('round')
 async def round_lizard(command, msg, user, channel, *args, **kwargs):
-    save_db('channel', command, msg, channel.id)
+    save_db('channel', 'round', msg, channel.id)
     return await status('status', msg, user, channel)
