888c903895268b32a79a432e19c274185388a007
Adam <adamtrayschool@gmail.com>
2022-01-22 17:19:48 -0600

    elevated commands locked behind botrole

--- bot/commands/botrole.js
+++ bot/commands/botrole.js
@@ -1,22 +1,14 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const utilities = require('util');
 const { getSetting } = require('../utilities/database/db_util');
-const { bold } = require('../utilities/utilities');
+const { bold, get_bot_role } = require('../utilities/utilities');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('botrole')
 		.setDescription('Returns Botrole information'),
 	async execute(interaction) {
-		const botrole = await getSetting('guild', 'botrole', interaction.guildId);
-		let botrole_name;
-
-		try {
-			botrole_name = interaction.guild.roles.cache.get(String(botrole)).name;
-		}
-		catch {
-			botrole_name = 'everyone';
-		}
+		const botrole_name = await get_bot_role(interaction);
 
 		await interaction.reply(utilities.format('The bot role is: %s', bold(botrole_name)));
 	},
